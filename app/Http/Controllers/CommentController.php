708a6a848dcad91ca6844fe90eb8501d86f075bb
iren <irasibil2@gmail.com>
2023-09-26 13:00:45 +0300

    implement sorting and pagination, setup hierarchy for them

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,14 +5,31 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CommentRequest;
 use App\Models\Comment;
 use App\Services\CommentService;
-
+use Illuminate\Http\Request;
 class CommentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $comments = Comment::with('user')->get()->toArray();
+        $childComments = Comment::with('user:id,name')
+                                ->whereNotNull('parent_id')
+                                ->get()
+                                ->toArray();
 
-        $comments = CommentService::handleFiles($comments);
+        $sort = $request->input('sort', 'created_at-asc');
+        list($sortField, $sortDirection) = explode('-', $sort);
+        $sortField = ($sortField === 'created_at') ? 'comments.'.$sortField : $sortField;
+
+        $topLevelComments = Comment::with('user:id,name')
+                                    ->select('comments.*', 'users.name')
+                                    ->whereNull('parent_id')
+                                    ->join('users', 'comments.user_id', '=', 'users.id')
+                                    ->orderBy($sortField, $sortDirection)
+                                    ->paginate(5)
+                                    ->items();
+
+        $allComments = array_merge($childComments, $topLevelComments);
+
+        $comments = CommentService::handleFiles($allComments);
         $comments = CommentService::buildHierarchy($comments);
 
         return response()->json($comments, 200);
